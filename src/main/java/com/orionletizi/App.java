eaaf95341be38f6ed606b3c65bc606508ca48cd8
Orion Letizi <oletizi@mac.com>
2016-04-20 14:53:52 -0700

    works

--- src/main/java/com/orionletizi/App.java
+++ src/main/java/com/orionletizi/App.java
@@ -1,27 +1,42 @@
 package com.orionletizi;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getByName("172.30.0.140", 80), 0);
-        server.createContext("/test", new MyHandler());
-        server.setExecutor(null); // creates a default executor
-        server.start();
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+
+public class App {
+  public static void main(String[] args) throws Exception {
+    String address = "172.30.0.140";
+    int port = 80;
+    if (args.length >= 2) {
+      address = args[0];
+      port = Integer.parseInt(args[1]);
     }
 
-    static class MyHandler implements HttpHandler {
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
-            OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }
+    info("Setting up server: address: " + address + ", port: " + port);
+    HttpServer server = HttpServer.create(new InetSocketAddress(InetAddress.getByName(address), port), 0);
+    server.createContext("/test", new MyHandler());
+    server.setExecutor(null); // creates a default executor
+    server.start();
+  }
+
+  private static void info(final Object o) {
+    System.out.println(App.class + ": " + o);
+  }
+
+  static class MyHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange t) throws IOException {
+      String response = "This is the response\n";
+      t.sendResponseHeaders(200, response.length());
+      OutputStream os = t.getResponseBody();
+      os.write(response.getBytes());
+      os.close();
     }
+  }
 }
